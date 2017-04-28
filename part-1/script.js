3a70053e846e6cbd0981d9c9cb27f673401d79f3
Jingwen Yang <jyang77@id.iit.edu>
2017-04-27 22:33:46 -0500

    Work done!

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,24 @@
 // Do your work in this file.
+
+var $divView = $( "<div class='view'></div>" );
+$( "body" ).append( $divView );
+
+var $divGrass = $( "<div class='grass'></div>" );
+var $divSun = $( "<div class='sun'></div>" );
+var $divMountain = $( "<div class='mountain'></div>" );
+$( ".view" ).append( $divGrass,$divSun,$divMountain );
+
+var $divMountainTop = $( "<div class='mountain-top'></div>" );
+$( ".mountain" ).append( $divMountainTop );
+
+var t = 1;
+while (t < 6) {
+    $("<div>").addClass("ray-" + t).appendTo( ".sun" );
+    t++;
+}
+
+var m = 1;
+do {
+    $("<div>").addClass("mountain-cap-" + m).appendTo( ".mountain" );
+    m++;
+} while (m < 4);
--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,15 @@
 // Do your work in this file.
+
+for (var i = 0; i < 196; i++) {
+    if (i % 2 == 0){
+        var $divBox = $( "<div class='box type-1'></div>" );
+    }
+    else{
+        var $divBox = $( "<div class='box type-2'></div>" );
+    }
+    $( "body" ).append( $divBox );
+}
+
+$( ".box" ).click(function() {
+  $( this ).toggleClass("clicked");
+});
